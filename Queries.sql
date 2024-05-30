0bc9d1f6f800cc5009ca59bf6f1095bd9c9aa662
Roni Pinus <160248285+roni45455@users.noreply.github.com>
2024-05-30 18:24:40 +0300

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -31,38 +31,51 @@ SET catagory =
    
    --check for new expense for possible new catagorize--
    SELECT * FROM credit_card.catagory c WHERE c.catagory  = 'Other' 
+
+
+   ---------------------------------------
+   -- Creation Of Monthly Budget Per Catagory Table --
+   ---------------------------------------
+   CREATE TABLE credit_card.budget (
+    category VARCHAR(50),
+    budget INT,
+    essential bool
+	);
+
+	INSERT INTO budget (category, budget, essential)
+	VALUES 
+    ('Bills', 500, True),
+    ('Groceries', 800, True),
+    ('Clothing shopping', 200, False),
+    ('Restaurants', 400, False),
+    ('Entertainment', 200, False),
+    ('Education', 1250, True),
+    ('Transportation', 200, True),
+    ('Self_care', 150, True),
+    ('Presents', 250, True),
+    ('Electronics shopping', 200, False),
+    ('Home',400,True); 
+
    
-  -------------------------------------
-  --show the expenses of chosen month--
-  -------------------------------------     
-   SELECT e.`date`, e.name , e.amount, c.catagory
-   FROM credit_card.expenses e join credit_card.catagory c on e.name = c.name 
-   WHERE YEAR(e.`date`) = '2023' AND MONTH(e.`date`) = '3' #chose year and month
-   ORDER BY e.`date`
-       
--------------------------------------------------------
---Total sum of expenses by catagory for certien month--
--------------------------------------------------------
-   SELECT MONTHNAME(e.`date`) as 'month' ,c.catagory, round(sum(e.amount),2) as 'sum'
-   from credit_card.expenses e join credit_card.catagory c on e.name = c.name
-   WHERE YEAR(e.`date`) = '2022' AND MONTH(e.`date`) = '6' #chose year and month
-   GROUP BY c.catagory, MONTHNAME(e.`date`)
-   
-   -----------------------------------------------------------------------------
-   --Compare monthly expense (of certin month) to same month of previous years--
-   -----------------------------------------------------------------------------
+   ---------------------------------------------------------
+   -- Overdraft from Budget Per Catagory For Certien Month --
+   -- Education is excluded because it's payment is not monthly--
+   ---------------------------------------------------------
    SELECT 
-   		YEAR(e.`date`) as 'Year',
-   		MONTHNAME(e.`date`) as 'Month', 
-   		ROUND(SUM(e.amount))  as 'sum'
-   FROM credit_card.expenses e 
-   WHERE MONTH(e.`date`) = '6' #chose month 
-   GROUP BY MONTHNAME(e.`date`),YEAR(e.`date`)
-   ORDER BY MIN(e.`date`);
+   		MONTHNAME(e.`date`) as 'month',
+   		c.catagory, round(sum(e.amount),2) as 'sum',
+   		b.budget as 'budget',
+   		(round(sum(e.amount),2) - b.budget) as Overdraft
+   from credit_card.expenses e 
+   		JOIN credit_card.catagory c on e.name = c.name
+   		JOIN credit_card.budget b on c.catagory = b.category
+   WHERE YEAR(e.`date`) = '2022' AND MONTH(e.`date`) = '5' AND c.catagory != 'Education' #chose year and month
+   GROUP BY c.catagory, MONTHNAME(e.`date`),b.budget
    
-   ----------------------------------------
-   --summary of monthly expanses per year--
-   ----------------------------------------
+  ------------------------------------
+   -- Monthly  overall expanses view --
+   ------------------------------------
+   CREATE VIEW Months as
    SELECT 
    		YEAR(e.`date`) as 'Year',
    		MONTHNAME(e.`date`) as 'Month', 
@@ -71,21 +84,50 @@ SET catagory =
    GROUP BY MONTHNAME(e.`date`),YEAR(e.`date`)
    ORDER BY MIN(e.`date`);
   
---------------------------------
---monthly avrage for each year--
---------------------------------
-WITH cte AS (
-   SELECT 
-   		YEAR(e.`date`) as `Year`,
-   		MONTHNAME(e.`date`) as `Month`, 
-   		ROUND(SUM(e.amount),2)  as `sum`
-   FROM credit_card.expenses e 
-   GROUP BY YEAR(e.`date`), MONTHNAME(e.`date`)
-)
-SELECT `Year`, AVG(`sum`) as `average_monthly_sum`
-FROM cte 
+  SELECT * FROM Months 
+  
+    ------------------------------------------------------------------------------
+   -- Compare monthly expense (of certin month) to same month of previous years --
+   ------------------------------------------------------------------------------
+  SELECT 
+    Year,
+    Month,
+    sum,
+    sum - LAG(sum) OVER (ORDER BY Year) AS Diff_from_last_year
+FROM Months
+WHERE Month = 'March'; 
+
+  
+  ---------------------------------
+  -- monthly avrage for each year--
+  ---------------------------------
+SELECT `Year`, round(AVG(`sum`),2) as `average_monthly_sum`
+FROM Months 
 GROUP BY `Year`;
 
+
+  --------------------------------------------------------
+  -- summary of monthly expanses per month per catagory --
+  --------------------------------------------------------
+   SELECT 
+   		YEAR(e.`date`) as 'Year',
+   		MONTHNAME(e.`date`) as 'Month', 
+   		c.catagory as category,
+   		ROUND(SUM(e.amount),2)  as 'sum'
+   FROM credit_card.expenses e JOIN credit_card.catagory c ON e.name = c.name
+   GROUP BY  YEAR(e.`date`), MONTHNAME(e.`date`), c.catagory
+   ORDER BY MIN(e.`date`);
+  
+       
+-------------------------------------------------------
+--Total sum of expenses by catagory for certien month--
+-------------------------------------------------------
+   SELECT MONTHNAME(e.`date`) as 'month' ,c.catagory, round(sum(e.amount),2) as 'sum'
+   from credit_card.expenses e join credit_card.catagory c on e.name = c.name
+   WHERE YEAR(e.`date`) = '2022' AND MONTH(e.`date`) = '6' #chose year and month
+   GROUP BY c.catagory, MONTHNAME(e.`date`)
+
+       
    ------------------------------------------------------
    --summary of monthly expanses per month per catagory--
    ------------------------------------------------------
